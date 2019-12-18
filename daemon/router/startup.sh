cc10f9af7e864acd0631774398be0eed929db4dd
Gaylor Bosson <gaylor.bosson@epfl.ch>
2019-12-18 22:14:36 +0100

    Fix the network calculation by adding the mask

--- daemon/router/startup.sh
+++ daemon/router/startup.sh
@@ -8,7 +8,7 @@ chmod 600 /dev/net/tun
 
 IP=$(ifconfig eth0 | grep "inet\b" | awk '{print $2}' | cut -d ":" -f 2)
 MASK=$(ifconfig eth0 | grep "inet\b" | awk '{print $4}' | cut -d ":" -f 2)
-NETWORK=$(ipcalc -n $IP | cut -d "=" -f 2)
+NETWORK=$(ipcalc -n $IP $MASK | cut -d "=" -f 2)
 
 # Allow the traffic to be forwarded to the LAN.
 iptables -t nat -I POSTROUTING -o eth0 -s 10.0.0.0/24 -j MASQUERADE
