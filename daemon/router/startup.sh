7d7f61d5273f034ff03b6ff57cfbd0f8db933a7c
Gaylor Bosson <gaylor.bosson@epfl.ch>
2019-12-20 13:45:20 +0100

    Fix router startup script

--- daemon/router/startup.sh
+++ daemon/router/startup.sh
@@ -16,7 +16,7 @@ iptables -t nat -I POSTROUTING -o eth0 -s 10.0.0.0/24 -j MASQUERADE
 # To find what is the cluster network, as it can change from a provider
 # to another, the routes to eth0 are looked up.
 
-CIDR=$(ip route show dev $DEVICE | grep -v default | grep -m 1 via | awk '{print $1}')
+CIDR=$(ip route show dev $NETDEV | grep -v default | grep -m 1 via | awk '{print $1}')
 NETWORK=$(ipcalc -n $CIDR | cut -d "=" -f 2)
 MASK=$(ipcalc -m $CIDR | cut -d "=" -f 2)
 
